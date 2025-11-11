fbfffb4d34fe0dcad22c06d05e58c755d3c92c7b
Evgheni <86593030+nev-front-dev@users.noreply.github.com>
2025-11-11 08:59:41 +0200

    added posthog config

--- app/components/providers/posthog-provider.js
+++ app/components/providers/posthog-provider.js
@@ -6,9 +6,7 @@ import { useEffect } from "react";
 
 export function PHProvider({ children }) {
   useEffect(() => {
-    const key =
-      process.env.NEXT_PUBLIC_POSTHOG_KEY ||
-      "phc_yM2yvy4tmvUDIXwYjowV0wskT8g19nfhNFVuwaR6JCM";
+    const key = "phc_yM2yvy4tmvUDIXwYjowV0wskT8g19nfhNFVuwaR6JCM";
 
     if (!key) {
       console.warn("PostHog key is missing");
@@ -16,9 +14,7 @@ export function PHProvider({ children }) {
     }
 
     posthog.init(key, {
-      api_host: "/ingest",
-      ui_host:
-        process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com",
+      api_host: "https://eu.i.posthog.com",
       person_profiles: "identified_only",
       capture_pageview: false,
       capture_pageleave: true,
