6d221761e8236200ee490dfe646f5256d1c2fbb2
Evgheni <86593030+nev-front-dev@users.noreply.github.com>
2025-11-14 10:00:22 +0200

    fixed letter animation

--- app/layout.js
+++ app/layout.js
@@ -54,6 +54,7 @@ export default function RootLayout({ children }) {
             <PostHogPageView />
         {children}
         <Script
+        id="to-top"
           dangerouslySetInnerHTML={{
             __html: `history.scrollRestoration = "manual"`,
           }}
