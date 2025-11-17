21a06045da533cf0822625ef7fe2fccc0b55109e
Evgheni <86593030+nev-front-dev@users.noreply.github.com>
2025-11-17 17:09:41 +0200

    updates

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,18 +3,7 @@ const config = {
     "@tailwindcss/postcss": {
       config: "./tailwind.config.js",
     },
-    'cssnano':
-      process.env.NODE_ENV === "production"
-        ? {
-            preset: [
-              "default",
-              {
-                discardComments: { removeAll: true },
-              },
-            ],
-          }
-        : false,
-  },
+  }
 };
 
 export default config;
